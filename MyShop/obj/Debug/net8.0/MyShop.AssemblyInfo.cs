497e04d57c344e0285993cc32ad2b96650c20662
imjonus <114491605+imjonus@users.noreply.github.com>
2024-09-17 14:30:01 +0200

    npm fiks

--- MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7447283771548914b0f107537dc56b9f5b081e03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14661f2b031102c3dba2f83ce22f44bfceb52309")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
