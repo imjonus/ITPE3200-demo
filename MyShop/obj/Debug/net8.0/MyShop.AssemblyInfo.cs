7efa143622d760469df175321eee1f2805fded16
imjonus <114491605+imjonus@users.noreply.github.com>
2024-09-20 12:07:06 +0200

    fix

--- MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fffb32d02f1f5c6a6d1a5e4498f13c2497624030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23401886fd4dcad3b43d3c4938424c91db756c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
